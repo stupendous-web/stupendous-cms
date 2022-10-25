9bae6513c2ef6a4f06221435dac658e12f7b3145
stupendous-web <topher@stupendousweb.com>
2022-10-25 09:57:23 -0700

    Think twice, code once.

--- pages/app/projects.js
+++ pages/app/projects.js
@@ -1,34 +1,39 @@
 import Link from "next/link";
 
+import Authentication from "../../components/Authentication";
 import Layout from "../../components/Layout";
 
 export default function Projects() {
   return (
-    <Layout>
-      <div className={"uk-section uk-section-small"}>
-        <div className={"uk-container uk-container-expand"}>
-          <div className={"uk-grid-match"} data-uk-grid={""}>
-            <div className={"uk-width-1-1"}>
-              <div>
-                <Link href={"/app/projects/create"}>
-                  <a className={"uk-button uk-button-primary uk-button-large"}>
-                    Create a Project
-                  </a>
-                </Link>
+    <Authentication>
+      <Layout>
+        <div className={"uk-section uk-section-small"}>
+          <div className={"uk-container uk-container-expand"}>
+            <div className={"uk-grid-match"} data-uk-grid={""}>
+              <div className={"uk-width-1-1"}>
+                <div>
+                  <Link href={"/app/projects/create"}>
+                    <a
+                      className={"uk-button uk-button-primary uk-button-large"}
+                    >
+                      Create a Project
+                    </a>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className={"uk-width-1-1"}>
-              <div
-                className={
-                  "uk-card uk-card-default uk-card-body uk-text-center uk-flex uk-flex-center uk-flex-middle"
-                }
-              >
-                asdf
+              <div className={"uk-width-1-1"}>
+                <div
+                  className={
+                    "uk-card uk-card-default uk-card-body uk-text-center uk-flex uk-flex-center uk-flex-middle"
+                  }
+                >
+                  asdf
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </Authentication>
   );
 }
