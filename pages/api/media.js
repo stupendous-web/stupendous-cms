e99c2503d9bf127e8bdb78e3ccf9a6f150ffa25c
stupendous-web <topher@stupendousweb.com>
2022-10-29 23:34:15 -0700

    Think twice, code once.

--- pages/api/media.js
+++ pages/api/media.js
@@ -1,13 +1,30 @@
 import middleware from "../../middleware/middleware";
 import nextConnect from "next-connect";
+const { Storage } = require("@google-cloud/storage");
 
 const handler = nextConnect();
 handler.use(middleware);
 
-export default handler.post(async (request, response) => {
-  console.log(request.files);
+export const uploadFile = async (file) => {
+  const storage = new Storage({
+    project_id: "stupendous-web",
+    credentials: {
+      client_email: process.env.GCS_CLIENT_EMAIL,
+      private_key: process.env.GCS_PRIVATE_KEY,
+    },
+  });
 
+  await storage.bucket("stupendous-cms").upload(file, {
+    destination: `test.${file.split(".").pop()}`,
+  });
+  console.log("Success!");
+};
+
+export default handler.post(async (request, response) => {
   const files = request?.files?.files;
+  console.log(files);
+
+  uploadFile(files[0]?.path).catch(console.error);
 
   response.status("200").send({ paths: files[0]?.path });
 });
