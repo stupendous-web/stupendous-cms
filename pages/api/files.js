5ec3abc41b04062235cdd1b469052262c02c1ae8
stupendous-web <topher@stupendousweb.com>
2022-10-30 16:02:56 -0700

    Think twice, code once.

--- pages/api/files.js
+++ pages/api/files.js
@@ -2,6 +2,18 @@ import { MongoClient, ObjectId } from "mongodb";
 const client = new MongoClient(process.env.MONGO_DB_URI);
 import { authOptions } from "./auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
+const { Storage } = require("@google-cloud/storage");
+
+export const deleteFile = async (fileId) => {
+  const storage = new Storage({
+    project_id: "stupendous-web",
+    credentials: {
+      client_email: process.env.GCS_CLIENT_EMAIL,
+      private_key: process.env.GCS_PRIVATE_KEY,
+    },
+  });
+  await storage.bucket("stupendous-cms").file(`${fileId}`).delete();
+};
 
 export default async function handler(request, response) {
   const body = request?.body;
@@ -32,14 +44,16 @@ export default async function handler(request, response) {
 
       break;
     case "DELETE":
-      await client
-        .db("stupendous-cms")
-        .collection("files")
-        .deleteOne({ _id: ObjectId(body?.fileId) })
-        .then(() =>
-          response.status(200).send("Good things come to those who wait.")
-        )
-        .finally(() => client.close());
+      deleteFile(body?.fileId).then(async () => {
+        await client
+          .db("stupendous-cms")
+          .collection("files")
+          .deleteOne({ _id: ObjectId(body?.fileId) })
+          .then(() =>
+            response.status(200).send("Good things come to those who wait.")
+          )
+          .finally(() => client.close());
+      });
 
       break;
     default:
