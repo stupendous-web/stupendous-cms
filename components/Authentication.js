ec8fa4aa6ce4d2530aac5140d471cb29a3f224e0
stupendous-web <topher@stupendousweb.com>
2022-11-09 17:52:59 -0800

    Think twice, code once.

--- components/Authentication.js
+++ components/Authentication.js
@@ -1,38 +1,53 @@
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { getSubscription } from "../utils/api";
+import { getBillingLink, getSubscription } from "../utils/api";
 
 export default function Authentication({ children }) {
   const [billingLink, setBillingLink] = useState();
-  const [subscription, setSubscription] = useState();
+  const [subscriptionStatus, setSubscriptionStatus] = useState();
 
   const { data: session } = useSession();
+  const user = session?.user;
 
   useEffect(() => {
-    session?.user?.stripeSubscription &&
-      getSubscription({ id: session?.user?.stripeSubscription }).then(
-        (response) => {
-          setSubscription(response.data);
-          console.log(response.data);
-        }
+    user?.stripeSubscription &&
+      getSubscription({ id: user?.stripeSubscription }).then((response) =>
+        setSubscriptionStatus(response?.data?.status)
       );
-  }, [session]);
+    getBillingLink({ stripeCustomer: session?.user?.stripeCustomer }).then(
+      (response) => setBillingLink(response?.data?.url)
+    );
+  }, [user]);
 
-  return session ? (
-    children
-  ) : (
-    <div
-      className={"uk-section-primary uk-flex uk-flex-center uk-flex-middle"}
-      data-uk-height-viewport={""}
-    >
-      <Link href={"/login"}>
-        <a>Login</a>
-      </Link>
-      &nbsp;|&nbsp;
-      <Link href={"/"}>
-        <a>Home</a>
-      </Link>
-    </div>
-  );
+  if (user && !["active", "trialing"].includes(subscriptionStatus))
+    return (
+      <div
+        className={"uk-section-primary uk-flex uk-flex-center uk-flex-middle"}
+        data-uk-height-viewport={""}
+      >
+        Your subscription may have expired. Visit your&nbsp;
+        <a href={billingLink}>billing portal</a>
+        &nbsp;to learn more or email topher@stupendousweb.com for support.
+      </div>
+    );
+
+  if (!user) {
+    return (
+      <div
+        className={"uk-section-primary uk-flex uk-flex-center uk-flex-middle"}
+        data-uk-height-viewport={""}
+      >
+        <Link href={"/login"}>
+          <a>Login</a>
+        </Link>
+        &nbsp;|&nbsp;
+        <Link href={"/"}>
+          <a>Home</a>
+        </Link>
+      </div>
+    );
+  }
+
+  return children;
 }
