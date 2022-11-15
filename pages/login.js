80fd9cb2d55ebcb5d323797ad8f8f4f29c747a97
stupendous-web <topher@stupendousweb.com>
2022-11-14 21:34:15 -0800

    Think twice, code once.

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import Image from "next/image";
 import { signIn } from "next-auth/react";
 
 import Navigation from "../components/Navigation";
 
+import login from "../images/undraw/undraw_login_re_4vu2.svg";
+
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState();
-
-  const router = useRouter();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -32,61 +31,75 @@ export default function Login() {
       </Head>
       <Navigation />
       <div style={{ height: "calc(100vh - 6rem)", overflow: "auto" }}>
-        <div className={"uk-section"}>
-          <div className={"uk-container uk-container-xsmall"}>
-            <form onSubmit={(event) => handleSubmit(event)}>
-              <h1>Login</h1>
-              <div className={"uk-margin"}>
-                <label className={"uk-form-label"}>Email</label>
-                <input
-                  type={"email"}
-                  value={email}
-                  className={"uk-input"}
-                  onChange={(event) => setEmail(event.target.value)}
-                  required
-                />
-              </div>
-              <div className={"uk-margin"}>
-                <label className={"uk-form-label"}>Password</label>
-                <input
-                  type={"password"}
-                  value={password}
-                  min={8}
-                  className={"uk-input"}
-                  onChange={(event) => setPassword(event.target.value)}
-                  required
-                />
-              </div>
-              {error && (
-                <div className={"uk-alert-danger"} data-uk-alert={""}>
-                  <p>
-                    {error?.title || "There was an error."} Please try again or
-                    email{" "}
-                    <Link
-                      href={"mailto:topher@stupendousweb.com"}
-                      legacyBehavior
-                    >
-                      <a>topher@stupendousweb.com</a>
-                    </Link>{" "}
-                    for help.
-                  </p>
+        <div className={"uk-grid-collapse"} data-uk-grid={""}>
+          <div
+            className={
+              "uk-width-2-3@s uk-flex uk-flex-center uk-flex-middle uk-visible@s"
+            }
+          >
+            <div className={"uk-width-medium"}>
+              <Image src={login} alt={"Login"} />
+            </div>
+          </div>
+          <div
+            className={"uk-width-1-3@s uk-section-muted uk-flex uk-flex-middle"}
+            data-uk-height-viewport={""}
+          >
+            <div className={"uk-width-1-1 uk-container uk-container-expand"}>
+              <form onSubmit={(event) => handleSubmit(event)}>
+                <h1>Login</h1>
+                <div className={"uk-margin"}>
+                  <label className={"uk-form-label"}>Email</label>
+                  <input
+                    type={"email"}
+                    value={email}
+                    className={"uk-input"}
+                    onChange={(event) => setEmail(event.target.value)}
+                    required
+                  />
                 </div>
-              )}
-              <input
-                type={"submit"}
-                value={"Let's go!"}
-                className={"uk-button uk-button-primary uk-margin-right"}
-              />
-              <Link href={"/register"} legacyBehavior>
-                <a
-                  title={
-                    "Get Started | Stupendous CMS by Stupendous Web | Clients manage their content. You manage their software."
-                  }
-                >
-                  Register
-                </a>
-              </Link>
-            </form>
+                <div className={"uk-margin"}>
+                  <label className={"uk-form-label"}>Password</label>
+                  <input
+                    type={"password"}
+                    value={password}
+                    min={8}
+                    className={"uk-input"}
+                    onChange={(event) => setPassword(event.target.value)}
+                    required
+                  />
+                </div>
+                {error && (
+                  <div className={"uk-alert-danger"} data-uk-alert={""}>
+                    <p>
+                      {error?.title || "There was an error."} Please try again
+                      or email{" "}
+                      <Link
+                        href={"mailto:topher@stupendousweb.com"}
+                        legacyBehavior
+                      >
+                        <a>topher@stupendousweb.com</a>
+                      </Link>{" "}
+                      for help.
+                    </p>
+                  </div>
+                )}
+                <input
+                  type={"submit"}
+                  value={"Let's go!"}
+                  className={"uk-button uk-button-primary uk-margin-right"}
+                />
+                <Link href={"/register"} legacyBehavior>
+                  <a
+                    title={
+                      "Get Started | Stupendous CMS by Stupendous Web | Clients manage their content. You manage their software."
+                    }
+                  >
+                    Register
+                  </a>
+                </Link>
+              </form>
+            </div>
           </div>
         </div>
       </div>
